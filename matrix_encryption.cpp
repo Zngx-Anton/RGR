86a2b8d64e139e1130747f750354870be5ba3150
Pilipenko Polina <125189325+Peresmeshnik01@users.noreply.github.com>
2023-05-20 00:44:14 +0700

    Add files via upload
    For example:
    Input: abcdefghijklmnop
    Output: jkgfeimnoplhdcba

---
+++ matrix_encryption.cpp
@@ -0,0 +1,57 @@
+// шифровка
+#include <iostream>  
+#include <string> 
+#include <math.h> 
+using namespace std;
+int main() {
+    string str = "abcdefghijklmnop";
+    int a;
+    a = sqrt(str.length());
+    char A[1000][1000];
+    int i, j, n = 0;
+    //Обработка и адресация каждого символа по отдельности 
+    for (int arr = 0; arr < sqrt(str.length()); arr++)
+    {
+        for (int idx = 0; idx < sqrt(str.length()); idx++)
+        {
+            A[arr][idx] = str[idx + (arr * sqrt(str.length()))];
+        }
+    }
+    if (a % 2 == 1) {
+        i = sqrt(str.length()) / 2, j = sqrt(str.length()) / 2;
+        do {
+            n++;
+            for (int k = 0; k < n; k++)
+                cout << A[i][j++];
+
+            if (n == sqrt(str.length()))
+                break;
+            for (int k = 0; k < n; k++)
+                cout << A[i--][j];
+            n++;
+            for (int k = 0; k < n; k++)
+                cout << A[i][j--];
+            for (int k = 0; k < n; k++)
+                cout << A[i++][j];
+        } while (n);
+    }
+    else {
+        i = 2, j = 1;
+        do {
+            n++;
+            for (int k = 0; k < n; k++)
+                cout << A[i][j++];
+            for (int k = 0; k < n; k++)
+                cout << A[i--][j];
+            n++;
+
+            for (int k = 0; k < n; k++)
+                cout << A[i][j--];
+
+            if (n == sqrt(str.length()))
+                break;
+            for (int k = 0; k < n; k++)
+                cout << A[i++][j];
+        } while (n);
+    }
+}
