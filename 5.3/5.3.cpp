fdee098f56dc94f2f784900bf7e1a2a5c55b54bb
vldpvnnk <133138277+vldpvnnk@users.noreply.github.com>
2023-06-09 13:23:34 +0700

    Update 5.3.cpp

--- 5.3/5.3.cpp
+++ 5.3/5.3.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <cstdlib>
 #include <fstream>
+#include <vector>
 #include <algorithm>   
 using namespace std; 
 // Функция шифрования
@@ -9,7 +10,7 @@ string encrypt(string plaintext, string key) {
     string ciphertext = "";
 
     // Создаем таблицу замены
-    char table[26][26];
+    vector<vector<char>> table(26, vector<char>(26));
     for (int i = 0; i < 26; i++) {
         for (int j = 0; j < 26; j++) {
             table[i][j] = 'A' + (i + j) % 26;
@@ -23,13 +24,15 @@ string encrypt(string plaintext, string key) {
         }
     }
 
-    // Шифруем каждую букву открытого текста
+    int col = 0;
+
+    // Шифруем каждый символ открытого текста
     for (char c : plaintext) {
         if (isalpha(c)) {
             c = toupper(c);
-            int row = key_only_letters[c-'A'] - 'A';
-            int col = rand() % 26;
-            ciphertext += table[row][col];
+            int row = key_only_letters[col % key_only_letters.length()] - 'A';
+            ciphertext += table[row][c - 'A'];
+            col++;
         }
     }
 
@@ -41,7 +44,7 @@ string decrypt(string ciphertext, string key) {
     string plaintext = "";
 
     // Создаем таблицу замены
-    char table[26][26];
+    vector<vector<char>> table(26, vector<char>(26));
     for (int i = 0; i < 26; i++) {
         for (int j = 0; j < 26; j++) {
             table[i][j] = 'A' + (i + j) % 26;
@@ -55,22 +58,26 @@ string decrypt(string ciphertext, string key) {
         }
     }
 
-    // Расшифровываем каждую букву шифротекста
+    int col = 0;
+
+    // Расшифровываем каждый символ шифротекста
     for (char c : ciphertext) {
         if (isalpha(c)) {
             c = toupper(c);
-            int row = key_only_letters[c-'A'] - 'A';
+            int row = key_only_letters[col % key_only_letters.length()] - 'A';
             for (int i = 0; i < 26; i++) {
                 if (table[row][i] == c) {
                     plaintext += 'A' + i;
                     break;
                 }
             }
+            col++;
         }
     }
 
     return plaintext;
 }
+
 
 string get_password() { 
     string password; 
@@ -93,56 +100,63 @@ string read_from_file(string filename) {
     return text; 
 } 
 
-int main() {
-    setlocale(LC_ALL,"RUS");
-    string message, key, password;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string message, password,key;
     char answer;
-    
-    cout << "Введите исходный текст: "; 
-    getline(cin, message); 
- 
-    write_to_file("source.txt", message); 
- 
-    password = get_password(); 
-    
-    if (password == "secret") { 
-        message = read_from_file("source.txt"); 
-        message = encrypt(message,key); 
-        write_to_file("encoded.txt", message); 
-        cout << "Текст зашифрован и записан в encoded.txt" << endl; 
+    bool all_upper = true;
+    cout << "Введите исходный текст: ";
+    getline(cin, message);
+    for(char c: message){
+        if(islower(c)){
+            all_upper = false;
+            cout <<"Ошибка: строка содержит символы нижнего регистра.\n";
+            break;
+        }
     }
-else { 
-        cout << "Неверный пароль, шифрование невозможно" << endl; 
+    if (all_upper){
+        cout << "Введите ключ: ";
+        getline(cin,key);
+    
+        write_to_file("source.txt", message);
+        password = get_password();
+        // шифрование   
+        if (password == "secret") {
+            message = read_from_file("source.txt");
+            message = encrypt(message, key);
+            write_to_file("encoded_message.txt", message);
+            cout << "Текст зашифрован и записан в encoded_message.txt" << endl;
+        }
+        else {
+            cout << "Неверный пароль, шифрование невозможно" << endl;
+        }
+    
+        password = get_password();
+        // дешифрование   
+        if (password == "secret") {
+            message = read_from_file("encoded_message.txt");
+            message = decrypt(message, key);
+            write_to_file("decoded_message.txt", message);
+            cout << "Текст расшифрован и записан в decoded_message.txt" << endl;
+        }
+        else {
+            cout << "Неверный пароль, дешифрование невозможно" << endl;
+        }
+        cout << "Нажмите 'p', чтобы распечатать файлы: ";
+        cin >> answer;
+        if (answer == 'p' || answer == 'P') {
+            cout << "Файл source.txt:" << endl;
+            cout << endl;
+            cout << read_from_file("source.txt") << endl;
+            cout << endl;
+            cout << "Файл encoded_message.txt:" << endl;
+            cout << endl;
+            cout << read_from_file("encoded_message.txt") << endl;
+            cout << endl;
+            cout << "Файл decoded_message.txt:" << endl;
+            cout << endl;
+            cout << read_from_file("decoded_message.txt") << endl;
+        }
     }
-    
-    password = get_password();
-    
-    // дешифрование   
-    if (password == "secret") { 
-        message = read_from_file("encoded.txt"); 
-        message = decrypt(message,key); 
-        write_to_file("decoded.txt", message); 
-        cout << "Текст расшифрован и записан в decoded.txt" << endl; 
-    } 
-    else { 
-        cout << "Неверный пароль, дешифрование невозможно" << endl; 
-    }
-    
-    cout << "Нажмите 'p', чтобы распечатать файлы: "; 
-    cin >> answer;
-    
-    if (answer == 'p' || answer == 'P') { 
-        cout << "Файл source.txt:" << endl; 
-        cout << endl; 
-        cout << read_from_file("source.txt") << endl; 
-        cout << endl; 
-        cout << "Файл encoded.txt:" << endl; 
-        cout << endl; 
-        cout << read_from_file("encoded.txt") << endl; 
-        cout << endl; 
-        cout << "Файл decoded.txt:" << endl; 
-        cout << endl; 
-        cout << read_from_file("decoded.txt") << endl; 
-    } 
-    return 0; 
 }
